ef48e044974ae3bf250c642c00a468a5c4683a9b
huuphuoc <huuphuoc>
2024-09-02 01:04:14 +0700

    workspace service

--- src/workspaces/workspaces.service.ts
+++ src/workspaces/workspaces.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/entities/user.entity';
 import { Workspace } from 'src/entities/workspace.entity';
 import { UsersService } from 'src/users/users.service';
-import { Repository } from 'typeorm';
+import { Like, Repository } from 'typeorm';
 
 @Injectable()
 export class WorkspacesService {
@@ -11,4 +12,37 @@ export class WorkspacesService {
     @InjectRepository(Workspace)
     private workspaceRepository: Repository<Workspace>,
   ) {}
+
+  async findAll(user: User) {
+    return this.workspaceRepository.find({
+      where: {
+        id: user.id,
+      },
+    });
+  }
+
+  async findByName(user: User, name: string) {
+    return this.workspaceRepository.find({
+      where: {
+        id: user.id,
+        name: Like(`%${name}%`),
+      },
+    });
+  }
+
+  async createWorkspace(name: string, user: User) {
+    const workspace = this.workspaceRepository.create({
+      owner: user,
+      name: name,
+    });
+    return this.workspaceRepository.save(workspace);
+  }
+
+  async renameWorkspace(name: string, workspaceId: number) {
+    return this.workspaceRepository.update({ id: workspaceId }, { name });
+  }
+
+  async deleteWorkspace(workspaceId: number) {
+    return this.workspaceRepository.delete({ id: workspaceId });
+  }
 }
